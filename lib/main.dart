439cabb4c2ae46005e2fa8ee729ba3c94304bcd1
wiseah <hyunah2765@gmail.com>
2024-08-22 06:02:35 +0900

    Sacfold 활용해서 연습하기

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Image.asset('assets/grape.png')
-    );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('앱임'),
+          centerTitle: false,
+          titleTextStyle: TextStyle(
+            fontSize: 20, color: Colors.white
+          ),
+          backgroundColor: Colors.blue,
+        ), //상단에 들어갈 위젯
+        body: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            width: double.infinity, height: 150,
+            // margin: EdgeInsets.fromLTRB(20, 20, 0, 0),
+            // padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.purple,
+                border: Border.all(color: Colors.black)
+
+            ),
+            child: Text('안녀엉'),
+          ),
+        ), //중단에 들어갈 위젯
+
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            boxShadow: const <BoxShadow>[
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10,
+              )
+            ]
+          ),
+
+        child: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const[
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ],
+            ),
+          ),
+        ),
+        ),
+      );
   }
 }
